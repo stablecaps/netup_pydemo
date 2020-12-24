5e23fc7eb0655ee68a9af435057d01b865880956
meatware <wetware@zoho.com>
2020-12-24 23:05:01 +0000

    add full- and subroutine launcher

--- netup_luncher.py
+++ netup_luncher.py
@@ -1,11 +1,12 @@
 """Main entrpoint to run code taht checks and evaluates reasons for loss in internet connectivity."""
 
-import os
 import sys
+import argparse
 from components.helpers import run_cmd_with_errorcode
 from components.printers import fmt_error_bold_red
 from components.connectivity_checker import check_connectivity_main, check_publicip_main
 from components.dnserver_check import dns_check_main
+from components.traceroute import traceroute_main
 
 # Strategy:
 # Start with higher level requests, then move towards more granular tests
@@ -21,30 +22,102 @@ from components.dnserver_check import dns_check_main
 # 5. check dns servers in
 
 
-if __name__ == "__main__":
+class NetupLauncher:
+    """
+    Parse command line options and select netup launch mode.
+    """
 
-    # TODO: cli-options
-    # print_all_ifaces - nmcli_printer()
-    #
+    def __init__(self):
+        help_banner = "Available command options: <all|connx|publicip|dns|traceroute>"
 
-    #################################################################
-    ### 1. Simple ping-like connectivity test for a connection
-    tcp_ping_commm = "nc -vz -w 5 www.google.com 8080"
-    tcp_ping = run_cmd_with_errorcode(comm_str=tcp_ping_commm)
+        parser = argparse.ArgumentParser(
+            description="AWS MP Framework launcher",
+            usage="python netup_luncher.py <command> <options>",
+        )
 
-    if not tcp_ping:
-        fmt_error_bold_red(mystr="TCP Ping failed. Now checking connection settings..")
-        #################################################################
-        ### 2. Check basic connectivity to internet
-        active_nmcli_dict = check_connectivity_main()
+        parser.add_argument("command", help=help_banner)
 
-        ################
-        ## 3. Check whether public IP is assigned
+        # parse_args defaults to [1:] for args, but you need to
+        # exclude the rest of the args too, or validation will fail
+        args = parser.parse_args(sys.argv[1:4])
+        if not hasattr(NetupLauncher, args.command):
+            print("Unrecognized command")
+            parser.print_help()
+            sys.exit(1)
+
+        # use dispatch pattern to invoke method with same name
+        getattr(self, args.command)()
+
+    @staticmethod
+    def connx():
+        """Launch subroutine to check connection details using route & nmcli"""
+
+        print("\nLaunching route & nmcli subroutine...\n")
+        check_connectivity_main()
+        print("\nExiting.")
+
+    @staticmethod
+    def publicip():
+        """Launch subroutine to check publicip"""
+
+        print("\nLaunching publicip subroutine...\n")
         check_publicip_main()
 
-        ################
-        ## 4. Check traceroute
+        print("\nExiting.")
 
-        ################
-        ## 5. Check DNS servers
+    @staticmethod
+    def dns():
+        """Launch subroutine to check DNS Servers"""
+
+        print("\nLaunching DNS Server check subroutine...\n")
+        active_nmcli_dict = check_connectivity_main()
         dns_check_main(active_nmcli_dict=active_nmcli_dict)
+
+        print("\nExiting.")
+
+    @staticmethod
+    def traceroute():
+        """Launch subroutine to perform traceroute"""
+
+        print("\nLaunching traceroute check subroutine...\n")
+        traceroute_main()
+
+        print("\nExiting.")
+
+    @staticmethod
+    def all():
+        """Launch all routines to detect internet connectivity issues"""
+
+        print("\nLaunch all routines\n")
+        #################################################################
+        ### 1. Simple ping-like connectivity test for a connection
+        # TODO: this can be set to use port 8080 to delibrately fail for testing purposes
+        tcp_ping_commm = "nc -vz -w 5 www.google.com 8080"
+        tcp_ping = run_cmd_with_errorcode(comm_str=tcp_ping_commm)
+
+        if not tcp_ping:
+            fmt_error_bold_red(
+                mystr="TCP Ping failed. Now checking connection settings.."
+            )
+            #################################################################
+            ### 2. Check basic connectivity to internet
+            active_nmcli_dict = check_connectivity_main()
+
+            ################
+            ## 3. Check whether public IP is assigned
+            check_publicip_main()
+
+            ################
+            ## 4. Check traceroute
+            traceroute_main()
+
+            ################
+            ## 5. Check DNS servers
+            dns_check_main(active_nmcli_dict=active_nmcli_dict)
+
+            print("\nExiting.")
+
+
+if __name__ == "__main__":
+
+    NetupLauncher()
