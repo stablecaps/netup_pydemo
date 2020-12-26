9d8c539a90e9b2d6b774e0836b90b8e6ef4d05bd
meatware <wetware@zoho.com>
2020-12-26 23:14:58 +0000

    put printers into class & tidy up connectivity_checker module

--- components/connectivity_checker.py
+++ components/connectivity_checker.py
@@ -8,12 +8,7 @@ from components.helpers import (
     substr_dict_key_search,
     process_subp_output,
 )
-from components.printers import (
-    print_dict_results,
-    fmt_bold_yellow,
-    fmt_bold_red,
-    nmcli_printer,
-)
+from components.printers import ColourPrinter
 
 
 def get_iface_info():
@@ -51,13 +46,13 @@ def get_nmcli_info():
 
     nmcli_dict = {}
     for subli in nmcli_info:
-        # print(subli)
 
         assert len(subli) == 2, "nmcli split error. script assumes 2 values per line"
 
         nmkey = subli[0]
         nmval = subli[1]
         if nmkey == "GENERAL.DEVICE":
+            ### Create a new key with empty list ready to be appended to
             dict_key = nmval
             nmcli_dict[dict_key] = []
         else:
@@ -66,14 +61,12 @@ def get_nmcli_info():
     return nmcli_dict
 
 
-def check_connx_main():
-    """
-    Subroutine to check connection details using route & nmcli.
-    """
+def run_iface_subroutine():
+    prt = ColourPrinter()
 
     iface_dict = get_iface_info()
 
-    print_dict_results(
+    prt.print_dict_results(
         results_dict=iface_dict,
         header="Kernel IP routing table",
         fmt_func_str="fmt_bold_col1",
@@ -82,7 +75,7 @@ def check_connx_main():
     default_iface = iface_dict.get("0.0.0.0", None)
 
     if default_iface is None:
-        fmt_bold_red(
+        prt.fmt_bold_red(
             mystr=(
                 "\nNo default gateway detected."
                 + "\nPlease check network cable/connection."
@@ -90,17 +83,19 @@ def check_connx_main():
             )
         )
         sys.exit(1)
+    prt.fmt_bold_yellow(mystr=f"Default iface is {default_iface}")
 
-    fmt_bold_yellow(mystr=f"Default iface is {default_iface}")
+    return default_iface
 
-    ###
+
+def run_nmcli_subroutine(default_iface):
+    """
+    Get connection info for main network interface.
+    """
+
+    prt = ColourPrinter()
+
     nmcli_all_dict = get_nmcli_info()
-
-    nmcli_printer(
-        nmcli_all_dict=nmcli_all_dict,
-        default_iface=default_iface,
-        print_all_ifaces=False,
-    )
 
     active_nmcli_dict = list_of_2nelem_lists_2dict(
         list_of_2nelem_lists=nmcli_all_dict[default_iface]
@@ -108,62 +103,80 @@ def check_connx_main():
 
     default_gateway = active_nmcli_dict.get("IP4.GATEWAY", None)
 
+    priv_ip_addresses = substr_dict_key_search(
+        in_dict=active_nmcli_dict, search_term="IP4.ADDRESS"
+    )
+
+    prt.nmcli_printer(
+        nmcli_all_dict=nmcli_all_dict,
+        default_iface=default_iface,
+        print_all_ifaces=False,
+    )
+
     if default_gateway is None:
-        fmt_bold_red(
+        prt.fmt_bold_red(
             mystr="Cannot find default gateway. Possible configuration error.\n Exiting..."
         )
         sys.exit(1)
 
-    fmt_bold_yellow(mystr=f"Default gateway is {default_gateway}")
-
-    priv_ip_addresses = substr_dict_key_search(
-        in_dict=active_nmcli_dict, search_term="IP4.ADDRESS"
-    )
+    prt.fmt_bold_yellow(mystr=f"Default gateway is {default_gateway}")
 
     if len(priv_ip_addresses) > 0:
         pip_str = ""
         for priv_ip in priv_ip_addresses:
             pip_str += f"\tPRIVATE IP: {priv_ip}\n"
 
-        fmt_bold_yellow(mystr=f"Detected private Ip Addresses:\n{pip_str}")
+        prt.fmt_bold_yellow(mystr=f"Detected private Ip Addresses:\n{pip_str}")
 
-    print("active_nmcli_dict", active_nmcli_dict)
     return active_nmcli_dict
 
 
-def check_publicip_main():
+def run_publicip_routine():
     """
     Main routine to check:
     1.  users basic connection details & gateway (router) situation
     2.  users public IP address can be retrived via an https lookup or alternatively via dig
     """
 
+    prt = ColourPrinter()
+
     public_ip_https = whatis_publicip(ip_check_url="https://ipinfo.io/ip", timeout=2)
 
+    # dig @resolver4.opendns.com myip.opendns.com +short
+    # dig @8.8.8.8 -t txt o-o.myaddr.l.google.com +short
+    # TODO: add alts
+    public_ip_dig = run_cmd_with_output(
+        comm_str="dig @208.67.220.222 myip.opendns.com +short"
+    )
+
     if public_ip_https is None:
-        fmt_bold_red(mystr="Cannot retrieve your public IP address over HTTPS.")
-
-        print("Now checking via dig")
-
-        # dig @resolver4.opendns.com myip.opendns.com +short
-        public_ip_dig = run_cmd_with_output(
-            comm_str="dig @208.67.220.222 myip.opendns.com +short"
-        )
-
-        ############################################################
-        if not public_ip_dig:
-            fmt_bold_red(
-                mystr=(
-                    "Cannot retrieve your public IP address via dig."
-                    + "\n It is likely that the connection to your ISP is broken."
-                )
-            )
-        else:
-            fmt_bold_yellow(
-                mystr=f"Your public IP (via dig) is {public_ip_dig}"
-                + "\nTherefore TCP connectivity is limited"
-            )
-            # TODO: Decide whether to exit here
-        ############################################################
+        prt.fmt_bold_red(mystr="Cannot retrieve your public IP address over HTTPS.")
     else:
-        fmt_bold_yellow(mystr=f"Your public IP (over HTTPS) is {public_ip_https}")
+        prt.fmt_bold_yellow(mystr=f"Your public IP (HTTPS) is {public_ip_https}")
+    ############################################################
+    if not public_ip_dig:
+        prt.fmt_bold_red(
+            mystr=(
+                "Cannot retrieve your public IP address via dig (UDP)."
+                + "\n It is likely that the connection to your ISP is broken."
+            )
+        )
+    else:
+        prt.fmt_bold_yellow(
+            mystr=f"Your public IP via dig (UDP) is {public_ip_dig.decode()}"
+        )
+    ############################################################
+
+
+def check_connx_main():
+    """
+    Subroutine to check connection details using route & nmcli.
+    """
+
+    default_iface = run_iface_subroutine()
+
+    active_nmcli_dict = run_nmcli_subroutine(default_iface=default_iface)
+
+    run_publicip_routine()
+
+    return active_nmcli_dict
--- components/printers.py
+++ components/printers.py
@@ -5,154 +5,147 @@ from blessings import Terminal
 from components.helpers import list_of_2nelem_lists_2dict
 
 
-def get_longest_str_in_dict(mydict, mode="keys"):
-    """
-    Return an integer corresponding to the greatest number of characters
-    from all the keys or values in a dictionary.
-    `mode` sets whether keys or values are searched.
-    """
+class ColourPrinter:
+    def __init__(self):
+        self.term = Terminal()
 
-    if mode == "keys":
-        dict_list = list(mydict.keys())
-    elif mode == "values":
-        dict_list = list(mydict.values())
-    else:
-        print("incorrect mode. Exiting..")
-        sys.exit(1)
+        ### Dictionary that holds format functions
+        self.fmt_func_dict = {
+            "fmt_ok_error": self.fmt_ok_error,
+            "fmt_keyok_valerror": self.fmt_keyok_valerror,
+            "fmt_ok_error_dns": self.fmt_ok_error_dns,
+            "fmt_bold_col1": self.fmt_bold_col1,
+        }
 
-    return len(max(dict_list, key=len))
+    def fmt_bold_red(self, mystr):
+        """
+        Return a string formatted in bold red.
+        """
 
+        print(f"\n{self.term.red}{self.term.bold}{mystr}{self.term.normal}")
 
-def fmt_bold_red(mystr):
-    """
-    Return a string formatted in bold red.
-    """
+    def fmt_bold_yellow(self, mystr):
+        """
+        Return a string formatted in bold yellow.
+        """
 
-    term = Terminal()
-    print(f"\n{term.red}{term.bold}{mystr}{term.normal}")
+        print(f"\n{self.term.yellow}{self.term.bold}{mystr}{self.term.normal}")
 
+    def get_longest_str_in_dict(self, mydict, mode="keys"):
+        """
+        Return an integer corresponding to the greatest number of characters
+        from all the keys or values in a dictionary.
+        `mode` sets whether keys or values are searched.
+        """
 
-def fmt_bold_yellow(mystr):
-    """
-    Return a string formatted in bold yellow.
-    """
-
-    term = Terminal()
-    print(f"\n{term.yellow}{term.bold}{mystr}{term.normal}")
-
-
-def fmt_ok_error(results_dict):
-    """
-    Return a key value pair with value formatted in red if it does not contain
-    the substring "OK".
-    Key is always green bold.
-    """
-
-    term = Terminal()
-
-    max_spaces = get_longest_str_in_dict(mydict=results_dict, mode="keys")
-    for key, value in results_dict.items():
-        key_just = (key + ":").ljust(max_spaces, " ")
-        if "OK - " in value:
-            print(f"{term.green}{term.bold}{key_just} \t{term.normal} {value}")
+        if mode == "keys":
+            dict_list = list(mydict.keys())
+        elif mode == "values":
+            dict_list = list(mydict.values())
         else:
+            print("incorrect mode. Exiting..")
+            sys.exit(1)
+
+        return len(max(dict_list, key=len))
+
+    def fmt_ok_error(self, results_dict):
+        """
+        Return a key value pair with value formatted in red if it does not contain
+        the substring "OK".
+        Key is always green bold.
+        """
+
+        max_spaces = self.get_longest_str_in_dict(mydict=results_dict, mode="keys")
+        for key, value in results_dict.items():
+            key_just = (key + ":").ljust(max_spaces, " ")
+            if "OK - " in value:
+                print(
+                    f"{self.term.green}{self.term.bold}{key_just} \t{self.term.normal} {value}"
+                )
+            else:
+                print(
+                    f"{self.term.bold}{self.term.green}{key_just} {self.term.normal}\t{self.term.red}{value}{self.term.normal}"
+                )
+
+    def fmt_keyok_valerror(self, results_dict):
+        """
+        Return a key value pair with value always formatted in red.
+        Key is always green bold.
+        """
+
+        max_spaces = self.get_longest_str_in_dict(mydict=results_dict, mode="keys")
+        for key, value in results_dict.items():
+            key_just = (key + ":").ljust(max_spaces, " ")
             print(
-                f"{term.bold}{term.green}{key_just} {term.normal}\t{term.red}{value}{term.normal}"
+                f"{self.term.bold}{self.term.green}{key_just} {self.term.normal}\t{self.term.red}{value}{self.term.normal}"
             )
 
+    def fmt_ok_error_dns(self, results_dict):
+        """
+        Return a key value pair with value formatted in red if it does not contain
+        the substring starts with "The DNS".
+        Key is always green bold.
+        """
 
-def fmt_keyok_valerror(results_dict):
-    """
-    Return a key value pair with value always formatted in red.
-    Key is always green bold.
-    """
+        # TODO: see if this can be combined with fmt_ok_error()
 
-    term = Terminal()
+        max_spaces = self.get_longest_str_in_dict(mydict=results_dict, mode="keys")
+        for key, value in results_dict.items():
+            key_just = (key + ":").ljust(max_spaces, " ")
+            if value.startswith("The DNS"):
+                print(
+                    f"{self.term.green}{self.term.bold}{key_just} {self.term.normal}\t{self.term.red}{value}{self.term.normal}"
+                )
+            else:
+                print(
+                    f"{self.term.green}{self.term.bold}{key_just} \t{self.term.normal} {value}"
+                )
 
-    max_spaces = get_longest_str_in_dict(mydict=results_dict, mode="keys")
-    for key, value in results_dict.items():
-        key_just = (key + ":").ljust(max_spaces, " ")
+    def fmt_bold_col1(self, results_dict):
+        """
+        Return a key value pair with value always normal font.
+        Key is always green bold.
+        All text is green
+        """
+
+        max_spaces = self.get_longest_str_in_dict(mydict=results_dict, mode="keys")
+        for key, value in results_dict.items():
+            key_just = (key + ":").ljust(max_spaces, " ")
+            print(
+                f"{self.term.green}{self.term.bold}{key_just} \t{self.term.normal}{value}"
+            )
+
+    def print_dict_results(self, results_dict, header, fmt_func_str):
+        """
+        Prints out colour coded results from dictionary with user defined header.
+        `fmt_func_str` is used to lookup functions stored in `fmt_func_dict`
+        """
+
         print(
-            f"{term.bold}{term.green}{key_just} {term.normal}\t{term.red}{value}{term.normal}"
+            f"\n {self.term.bold}{self.term.underline}>> {header}: {self.term.normal}"
         )
+        self.fmt_func_dict[fmt_func_str](results_dict)
 
+    ###################################################################
 
-def fmt_ok_error_dns(results_dict):
-    """
-    Return a key value pair with value formatted in red if it does not contain
-    the substring starts with "The DNS".
-    Key is always green bold.
-    """
+    def nmcli_printer(self, nmcli_all_dict, default_iface, print_all_ifaces=False):
+        """
+        Prints out colour coded results from dictionary containing nmcli data.
+        Option `print_all_ifaces` prints all interface data.
+        By default only the default interface is printed out.
+        """
 
-    # TODO: see if this can be combined with fmt_ok_error()
-    term = Terminal()
+        iface_name_li = [default_iface]
+        if print_all_ifaces:
+            iface_name_li = list(nmcli_all_dict.keys())
 
-    max_spaces = get_longest_str_in_dict(mydict=results_dict, mode="keys")
-    for key, value in results_dict.items():
-        key_just = (key + ":").ljust(max_spaces, " ")
-        if value.startswith("The DNS"):
-            print(
-                f"{term.green}{term.bold}{key_just} {term.normal}\t{term.red}{value}{term.normal}"
+        for iface in iface_name_li:
+            iface_dict = list_of_2nelem_lists_2dict(
+                list_of_2nelem_lists=nmcli_all_dict[iface]
             )
-        else:
-            print(f"{term.green}{term.bold}{key_just} \t{term.normal} {value}")
 
-
-def fmt_bold_col1(results_dict):
-    """
-    Return a key value pair with value always normal font.
-    Key is always green bold.
-    All text is green
-    """
-
-    term = Terminal()
-
-    max_spaces = get_longest_str_in_dict(mydict=results_dict, mode="keys")
-    for key, value in results_dict.items():
-        key_just = (key + ":").ljust(max_spaces, " ")
-        print(f"{term.green}{term.bold}{key_just} \t{term.normal}{value}")
-
-
-### Dictionary that holds format functions
-fmt_func_dict = {
-    "fmt_ok_error": fmt_ok_error,
-    "fmt_keyok_valerror": fmt_keyok_valerror,
-    "fmt_ok_error_dns": fmt_ok_error_dns,
-    "fmt_bold_col1": fmt_bold_col1,
-}
-
-
-def print_dict_results(results_dict, header, fmt_func_str):
-    """
-    Prints out colour coded results from dictionary with user defined header.
-    `fmt_func_str` is used to lookup functions stored in `fmt_func_dict`
-    """
-
-    term = Terminal()
-
-    print(f"\n {term.bold}{term.underline}>> {header}: {term.normal}")
-    fmt_func_dict[fmt_func_str](results_dict)
-
-
-###################################################################
-def nmcli_printer(nmcli_all_dict, default_iface, print_all_ifaces=False):
-    """
-    Prints out colour coded results from dictionary containing nmcli data.
-    Option `print_all_ifaces` prints all interface data.
-    By default only the default interface is printed out.
-    """
-
-    iface_name_li = [default_iface]
-    if print_all_ifaces:
-        iface_name_li = list(nmcli_all_dict.keys())
-
-    for iface in iface_name_li:
-        iface_dict = list_of_2nelem_lists_2dict(
-            list_of_2nelem_lists=nmcli_all_dict[iface]
-        )
-
-        print_dict_results(
-            results_dict=iface_dict,
-            header=f"Iface info for: {iface}",
-            fmt_func_str="fmt_bold_col1",
-        )
+            self.print_dict_results(
+                results_dict=iface_dict,
+                header=f"Iface info for: {iface}",
+                fmt_func_str="fmt_bold_col1",
+            )
